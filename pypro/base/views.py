62092e4b5a2cc30d3e1dd91a22ff704be630e856
k <daanrod93@gmail.com>
2022-07-13 05:53:31 -0300

    Landing Page Created

--- pypro/base/views.py
+++ pypro/base/views.py
@@ -1,9 +1,8 @@
-from django.http import HttpResponse
-# from django.shortcuts import render
+# from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 
 
 def home(request):
-    # raise ValueError()
-    return HttpResponse('<html><body>Hello Django</body></html>', content_type='text/html')
+    return render(request, 'base/home.html')
