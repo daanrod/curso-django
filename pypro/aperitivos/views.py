a094237546061dbe0719f91bdf8489abe3824c72
k <daanrod93@gmail.com>
2022-07-15 03:18:43 -0300

    Added filters to aperitivos titles

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    video = {'titulo': 'Video Aperitivo: Motivação',
-             'youtube_id': '2aYplgJrPDU',
-             }
+    videos = {
+        'motivacao': {'titulo': 'Video Aperitivo: Motivação', 'youtube_id': '2aYplgJrPDU'},
+        'instalacao-windows': {'titulo': 'Instalação Windows', 'youtube_id': 'hQayuyeEMy0'}
+    }
+    video = videos[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})
